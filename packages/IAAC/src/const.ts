5096b61024dd3db94de91bceb691b85902846393
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2023-08-06 22:55:19 +0800

    chore：删除 iaac 项目

--- packages/IAAC/src/const.ts
+++
@@ -1,9 +0,0 @@
-
-const BASE_URL = "http://localhost:3000"
-export const axiosConf = {
-  baseURL:BASE_URL,
-  headers:{
-    "Content-Type":"application/json"
-  },
-  timeout: 1000 * 30
-}
--- packages/IAAC/src/types/commomResponse.d.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosResponse } from "axios";
-export interface IResponse<T> extends AxiosResponse {
-  data: {
-    success: boolean;
-    code: number;
-    message: string;
-    result?: T;
-  };
-}
