c3a69175dbd9e133b9f08b68b50f024d2db03f96
taozhiyaoyao <taozhiyaoyao9527@outlook.com>
2023-08-11 18:13:22 +0800

    feat：初始化招新系统后台模板

---
+++ packages/enrollment-platform/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="h-screen ">
+    <el-container class="h-full">
+      <el-header class="border-b-1 border-b-solid border-b-[#dcdfe6]">
+        <Header />
+      </el-header>
+      <el-container class="h-full">
+        <el-aside width="200px">
+          <Sider />
+        </el-aside>
+        <el-container>
+          <el-main>
+            <Main />
+          </el-main>
+          <el-footer>
+            <Footer />
+          </el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ packages/enrollment-platform/src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import logo from '../../assets/gcu-womd.png'
+import avatar from "../../assets/vue.svg"
+</script>
+<template>
+  <div class="h-full flex justify-between items-center">
+    <div class="flex gap-x-4 items-center">
+      <img :src="logo" class="w-14" />
+      <h1>招新系统后台</h1>
+    </div>
+    <div class="flex gap-x-2 items-center">
+      <el-avatar :src="avatar" />
+      <el-dropdown>
+        <div class="flex gap-x-2 cursor-pointer focus-visible:outline-none">
+          <el-text size="large">Jammie Griffin</el-text>
+          <div class="i-ep:arrow-down"></div>
+        </div>
+        <template #dropdown>
+          <el-dropdown-item>站点导航</el-dropdown-item>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+<style scoped lang="scss"></style>
