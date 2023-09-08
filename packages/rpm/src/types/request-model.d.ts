adfeacc3afe7cc40dd612c188f8f63b510d35d99
Your nname <13232319788@163.com>
2023-09-08 20:14:16 +0800

    finshMember

--- packages/rpm/src/types/request-model.d.ts
+++ packages/rpm/src/types/request-model.d.ts
@@ -4,3 +4,37 @@ export interface NewWebsiteForm {
   websiteLogo: string
   belongs: string[]
 }
+// -----------------------------------
+
+//定义 获取成员列表 的请求参数对象类型
+export interface paramsGetMemberList{
+    departmentId:string
+    searchType:string
+    keyword:string
+    pageSize:number,
+    page:number
+}
+
+//定义 修改成员 的请求参数对象类型
+export interface paramsEditMembers{
+  username:string
+  name?:string
+  politicsStatus?:string
+  nickName?:string
+  avatar?:string
+  dorm?:string
+  major?:string
+  grade?:string
+  classNumber?:string
+  position?:string
+  phone?:string
+  email?:string
+}
+
+//定义 指定成员角色 的请求参数对象类型
+export interface paramsRole{
+  username:string
+  roles:Array<string>
+}
+
+
--- packages/rpm/src/types/response-data-model.d.ts
+++ packages/rpm/src/types/response-data-model.d.ts
@@ -3,6 +3,7 @@ export interface AlovaResponse<T> {
   data: T;
   message: string;
 }
+
 export interface DepartmentList {
   label: string;
   value: string;
@@ -45,4 +46,60 @@ export interface PermissionList {
     row: RouteListitem[];
     total: number;
   }
+}
+
+// ---------------------------------------
+export interface RowData{
+    username:string
+    name:string
+    phone:string
+    nickName:string
+    major:string
+    grade:string
+    classNumber:string
+    dorm:string
+    politicsStatus:string
+    avatar:string
+    position:string
+    email:string
+}
+//分页获取成员列表返回的data对象的全部数据类型限定（包括不展示的）
+export interface MembersInfo{
+  total:number
+  row:data[]
+}
+
+export interface InitMemberAllInfoClass{
+  total:number
+  row:RowData[]
+  page:1
+  pageSize:10
+}
+
+//获取成员角色返回数据的类型限定
+export interface RoleResponse {
+  timestamp: string,
+  status: number,
+  error: string,
+  path: string
+}
+export interface roleName{
+  roleId:string
+  roleName:string
+}
+
+export interface data{
+  username:string
+  name:string
+  phone:string
+  nickName:string
+  major:string
+  grade:string
+  classNumber:string
+  dorm:string
+  politicsStatus:string
+  avatar:string
+  position:string
+  email:string
+  roleName:string
 }
