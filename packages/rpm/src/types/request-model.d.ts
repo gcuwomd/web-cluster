1fb599430f55f40fcbff4160a2baf687d6fd3d16
Your nname <13232319788@163.com>
2023-09-09 11:57:18 +0800

    djr

--- packages/rpm/src/types/request-model.d.ts
+++ packages/rpm/src/types/request-model.d.ts
@@ -4,17 +4,37 @@ export interface NewWebsiteForm {
   websiteLogo: string
   belongs: string[]
 }
-export interface ChangeWebsiteInfForm extends NewWebsiteForm {
-  websiteId: string
-}
-export interface RouteForm {
-  readonly websiteId: string
-  routeUrl: string
-  routeDescription: string
-  routeId?: string
+// -----------------------------------
+
+//定义 获取成员列表 的请求参数对象类型
+export interface paramsGetMemberList{
+    departmentId:string
+    searchType:string
+    keyword:string
+    pageSize:number,
+    page:number
 }
 
-export interface RoleForm {
-  roleName: string;
-  readonly departmentId: string
+//定义 修改成员 的请求参数对象类型
+export interface paramsEditMembers{
+  username:string
+  name?:string
+  politicsStatus?:string
+  nickName?:string
+  avatar?:string
+  dorm?:string
+  major?:string
+  grade?:string
+  classNumber?:string
+  position?:string
+  phone?:string
+  email?:string
 }
+
+//定义 指定成员角色 的请求参数对象类型
+export interface paramsRole{
+  username:string
+  roles:Array<string>
+}
+
+
--- packages/rpm/src/types/response-data-model.d.ts
+++ packages/rpm/src/types/response-data-model.d.ts
@@ -3,6 +3,7 @@ export interface AlovaResponse<T> {
   data: T
   message: string
 }
+
 export interface DepartmentList {
   label: string
   value: string
@@ -20,6 +21,15 @@ export interface WebsiteBaseInfo {
 export interface WebsiteDetailInfo extends WebsiteBaseInfo {
   apiNum: number
   routeNumber: number
+}
+
+export interface RoleListItem {
+  roleId: string
+  roleName: string
+}
+export interface GetRoleList {
+  total: number
+  row: RoleListItem[]
 }
 
 export interface WebsiteApiInfo {
@@ -44,25 +54,71 @@ export type WebsitePermissions = {
   globalApi: WebsiteApiInfo[]
   routes: WebsiteRouteInfo[]
 }
-
-export interface RoleListItem {
-  roleId: string
-  roleName: string
-}
-export interface RoleList {
-  total: number
-  row: RoleListItem[]
-}
-
-export interface RoleInfo extends RoleListItem {
-  apiNumber: number
-  routeNumber: number
-  userNumber: number
-  websiteNumber: number
+export interface PermissionList {
+  row?: ApiListItem[] | RouteListitem [];
+  total?: number;
+  api?: {
+    row: ApiListItem[];
+    total:number;
+  }
+  route?:{
+    row: RouteListitem[];
+    total: number;
+  }
 }
 
-export type WebsiteOption = Pick<WebsiteBaseInfo, 'websiteId' | 'websiteName'> & {
-  disabled?: boolean
+// ---------------------------------------
+export interface RowData{
+    username:string
+    name:string
+    phone:string
+    nickName:string
+    major:string
+    grade:string
+    classNumber:string
+    dorm:string
+    politicsStatus:string
+    avatar:string
+    position:string
+    email:string
+}
+//分页获取成员列表返回的data对象的全部数据类型限定（包括不展示的）
+export interface MembersInfo{
+  total:number
+  row:data[]
 }
 
-export type AccessibleWebsite = Omit<WebsiteBaseInfo, 'departmentName'>
+export interface InitMemberAllInfoClass{
+  total:number
+  row:RowData[]
+  page:1
+  pageSize:10
+}
+
+//获取成员角色返回数据的类型限定
+export interface RoleResponse {
+  timestamp: string,
+  status: number,
+  error: string,
+  path: string
+}
+export interface roleName{
+  roleId:string
+  roleName:string
+}
+
+export interface data{
+  username:string
+  name:string
+  phone:string
+  nickName:string
+  major:string
+  grade:string
+  classNumber:string
+  dorm:string
+  politicsStatus:string
+  avatar:string
+  position:string
+  email:string
+  roleName:string
+}
